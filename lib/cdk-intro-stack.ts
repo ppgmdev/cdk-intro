e24cb4b0286bb618c781d6d88a6902d13d7992b5
Workshop Participant <participant@workshops.aws>
2024-10-06 22:18:54 +0000

    first commit

--- lib/cdk-intro-stack.ts
+++ lib/cdk-intro-stack.ts
@@ -1,16 +1,43 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { aws_dynamodb } from 'aws-cdk-lib';
+import { aws_lambda } from 'aws-cdk-lib';
+import { CfnOutput } from 'aws-cdk-lib';
 
 export class CdkIntroStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const ddb_table = new aws_dynamodb.TableV2(this, "DDBTable", {
+      partitionKey: {
+        name: "id",
+        type: aws_dynamodb.AttributeType.STRING,
+      },
+      removalPolicy: cdk.RemovalPolicy.DESTROY,
+    });
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'CdkIntroQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const lambda_bedrock = new aws_lambda.Function(this, "LambdaBedrock", {
+      code: aws_lambda.Code.fromAsset("./lambda/lambda_bedrock"),
+      handler: "index.handler",
+      runtime: aws_lambda.Runtime.PYTHON_3_12,
+      environment: {
+        "DDB_TABLE_NAME": ddb_table.tableName,
+      }
+    });
+
+    new CfnOutput(this, "LambdaARN", {
+      value: lambda_bedrock.functionArn,
+      description: "ARN of lambda function",
+    });
+    
+    new CfnOutput(this, "DDBName", {
+      value: ddb_table.tableName,
+      description: "Name of the DDB table",
+    });
+
+    new CfnOutput(this, "DDBArn", {
+      value: ddb_table.tableArn,
+      description: "ARN of the DDB Table",
+    });
   }
 }
